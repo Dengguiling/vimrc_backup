b09a3959b05acf0086732c9b71fe9256eecc7291
dengguiling <greens9@163.com>
2023-10-21 20:22:26 +0800

    add run.sh
    Signed-off-by: dengguiling <greens9@163.com>

---
+++ run.sh
@@ -0,0 +1,4 @@
+cp ~/.vim/plugged/sonokai/autoload/sonokai.vim ~/.vim/autoload/
+cp ~/.vim/plugged/sonokai/colors/ ~/.vim/ -r
+cp ~/.vim/plugged/sonokai/doc/ ~/.vim/doc/ -r
+cp ~/.vim/plugged/sonokai/autoload/airline ~/.vim/autoload/ -r
